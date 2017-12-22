07e662bbc835dbc3a024a22599e48032f17ac5f4
DESKTOP-JUNELLE\Junelle-PC <junelle@koreatech.ac.kr>
2017-12-22 20:07:51 +0900

    nothing

--- v1/readAdStudygroup.php
+++ v1/readAdStudygroup.php
@@ -10,13 +10,13 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 	$db = new DBOperations();
 
 	//$db->advertisingGroup();
-	
+
 	$response['error'] = false;
 	$response['num'] = $result['studygroup_num'];
 	$response['name'] = $result['studygroup_name'];
 	$response['purpose'] = $result['purpose'];
 	$response['host'] = $result['studygrouphost_email'];
-	$response['contents'] = $result['studygroup_contents'];	
+	$response['contents'] = $result['studygroup_contents'];
 } else {
 	$response['error'] = true;
 	$response['message'] = "Invalid Request";
